506e6bf0db6db8ee1fceb1ee8db9596d9ba768f0
bergstenarn <andersbergsten@yahoo.se>
2022-02-12 20:47:37 +0200

    Parameterize port

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get("/profile/:id", (req, res) => profile.handleProfileGet(res, req, db));
 app.put("/image", (req, res) => image.handleImage(req, res, db));
 app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log("App is listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 });
